e0c23c18df972c9f89c52f636470f30e850b94a2
jsymonowicz <joanna@symonowicz.pl>
2024-07-10 15:14:30 +0200

    Add solutions to 2 last problems in Basic Joins

--- LeetCode-Basic-Joins.sql
+++ LeetCode-Basic-Joins.sql
@@ -249,3 +249,88 @@ GROUP BY
     s.student_id, s.student_name, subj.subject_name
 ORDER BY 
     s.student_id, subj.subject_name;
+
+/*
+570. Managers with at Least 5 Direct Reports
+Find managers with at least 5 direct reports.
+*/
+
+CREATE TABLE Employee_2 (
+    id INT PRIMARY KEY,
+    name VARCHAR(20),
+    department VARCHAR(20),
+    managerId INT
+);
+
+INSERT INTO Employee_2 VALUES
+(101, 'John', 'A', null),
+(102, 'Dan', 'A', 101),
+(103, 'James', 'A', 101),
+(104, 'Amy', 'A', 101),
+(105, 'Anne', 'A', 101),
+(106, 'Ron', 'B', 101);
+
+-- Solution
+SELECT e2.name
+FROM Employee_2 e1
+JOIN Employee_2 e2
+ON e1.managerId = e2.id
+GROUP BY e2.id, e2.name
+HAVING COUNT(e1.id) >=5;
+
+-- Solution 2
+SELECT name
+FROM Employee_2
+WHERE id IN (
+    SELECT managerId
+    FROM Employee_2
+    GROUP BY managerId
+    HAVING COUNT(id) >= 5
+);
+
+/*
+1934. Confirmation Rate
+Confirmation rate of a user is the number of 'confirmed' messages divided by
+total number of requested confirmation messages. Confirmation rate of a user
+that did not request any confirmation messages is 0.
+Round the confirmation rate to two decimal places.
+Find the confirmation rate of each user.
+*/
+
+CREATE TABLE Signups (
+    user_id INT UNIQUE,
+    time_stamp DATETIME
+);
+
+CREATE TABLE Confirmations (
+    user_id INT,
+    time_stamp DATETIME,
+    action ENUM ('confirmed', 'timeout'),
+    PRIMARY KEY (user_id, time_stamp),
+    FOREIGN KEY (user_id) REFERENCES Signups(user_id) ON DELETE CASCADE
+);
+
+INSERT INTO Signups VALUES
+(3, '2020-03-21 10:16:13'),
+(7, '2020-01-04 13:57:59'),
+(2, '2020-07-29 23:09:44'),
+(6, '2020-12-09 10:39:37');
+
+INSERT INTO Confirmations VALUES
+(3, '2021-01-06 03:30:46', 'timeout'),
+(3, '2021-07-14 14:00:00', 'timeout'),
+(7, '2021-06-12 11:57:29', 'confirmed'),
+(7, '2021-06-13 12:58:28', 'confirmed'),
+(7, '2021-06-14 13:59:27', 'confirmed'),
+(2, '2021-01-22 00:00:00', 'confirmed'),
+(2, '2021-02-28 23:59:59', 'timeout');
+
+-- Solution
+SELECT 
+    s.user_id, 
+    ROUND(COALESCE(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(c.action), 0), 2)
+    AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
